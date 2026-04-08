ee26db459ecc4aedeb33de93a8642db030513994
Enrico Battistella <enrico.battistella@digitalrockers.it>
2026-04-08 15:30:15 +0200

    cartsrv api

--- src/app/services/cart-source.service.ts
+++ src/app/services/cart-source.service.ts
@@ -2,103 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { CartItem } from './../cart-item.entity';
 import { inject, Injectable, signal } from '@angular/core';
 
-const cart: CartItem[] = [
-  {
-    id: '1',
-    quantity: 2,
-    product: {
-      id: '1',
-      name: 'ssd',
-      netPrice: 95,
-      weight: 100,
-      discount: 5,
-    },
-  },
-  {
-    id: '2',
-    quantity: 1,
-    product: {
-      id: '2',
-      name: 'motherboard',
-      netPrice: 270,
-      weight: 900,
-      discount: 0
-    }
-  },
-  {
-    id: '3',
-    quantity: 2,
-    product: {
-      id: '3',
-      name: 'ram',
-      netPrice: 120,
-      weight: 60,
-      discount: 10,
-    }
-  },
-  {
-    id: '4',
-    quantity: 1,
-    product: {
-      id: '4',
-      name: 'processor',
-      netPrice: 400,
-      weight: 130,
-      discount: 0,
-    }
-  },
-  {
-    id: '5',
-    quantity: 1,
-    product: {
-      id: '5',
-      name: 'power supply',
-      netPrice: 130,
-      weight: 1400,
-      discount: 15,
-    }
-  },
-  {
-    id: '6',
-    quantity:1,
-    product: {
-      id: '6',
-      name: 'cpu cooler',
-      netPrice: 170,
-      weight: 1000,
-      discount: 23
-    }
-  },
-  {
-    id: '7',
-    quantity: 1,
-    product: {
-      id: '7',
-      name: 'gpu',
-      netPrice: 1600,
-      weight: 2500,
-      discount: 0
-    }
-  },
-  {
-    id: '8',
-    quantity: 1,
-    product: {
-      id: '8',
-      name: 'case',
-      netPrice: 130,
-      weight: 3500,
-      discount: 30
-    }
-  }
-];
 @Injectable({
   providedIn: 'root',
 })
 export class CartSourceService {
   private http = inject(HttpClient);
 
-  private internal = signal<CartItem[]>(cart);
+  private internal = signal<CartItem[]>([]);
   cart = this.internal.asReadonly();
 
   constructor() {
@@ -106,8 +16,8 @@ export class CartSourceService {
   }
 
   fetch() {
-    this.http.get('/api/cart-items').subscribe(items => {
-      console.log(items);
+    this.http.get<CartItem[]>('/api/cart-items').subscribe(items => {
+      this.internal.set(items);
     })
   }
 
@@ -115,22 +25,28 @@ export class CartSourceService {
     if (newQuantity < 0) {
       newQuantity = 0;
     }
-    const index = this.internal().findIndex(item => item.id === id);
-    if (index === -1) {
-      throw new Error(`Missing item with id ${id}`);
-    }
-    const clone = structuredClone(this.internal());
-    clone[index].quantity = newQuantity;
-    this.internal.set(clone);
+
+    this.http.put<CartItem>(`/api/cart-items/${id}`, { quantity: newQuantity })
+      .subscribe(updated => {
+        const index = this.internal().findIndex(item => item.id === id);
+        if (index === -1) {
+          throw new Error(`Missing item with id ${id}`);
+        }
+        const clone = structuredClone(this.internal());
+        clone[index] = updated;
+        this.internal.set(clone);
+      });
   }
 
   removeItem(id: string): void {
-    const index = this.internal().findIndex(item => item.id === id);
-    if (index === -1) {
-      throw new Error(`Missing item with id ${id}`);
-    }
-    const clone = structuredClone(this.internal());
-    clone.splice(index, 1);
-    this.internal.set(clone);
+    this.http.delete(`/api/cart-items/${id}`).subscribe(() => {
+      const index = this.internal().findIndex(item => item.id === id);
+      if (index === -1) {
+        throw new Error(`Missing item with id ${id}`);
+      }
+      const clone = structuredClone(this.internal());
+      clone.splice(index, 1);
+      this.internal.set(clone);
+    });
   }
 }

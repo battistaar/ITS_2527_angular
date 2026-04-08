535873895956a2ca54dde40fc6b0ff56d9fd626f
Enrico Battistella <enrico.battistella@digitalrockers.it>
2026-04-08 16:34:05 +0200

    checkout component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,36 +1,13 @@
-import { Component, inject } from '@angular/core';
-import { CartItem } from './cart-item.entity';
-import { FormsModule } from '@angular/forms';
-import { CartItemComponent } from './components/cart-item/cart-item.component';
-import { SummaryComponent } from './components/summary/summary.component';
-import { CartSourceService } from './services/cart-source.service';
-import { VatService } from './services/vat.service';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
   imports: [
-    FormsModule,
-    CartItemComponent,
-    SummaryComponent
-  ]
+    RouterOutlet
+]
 })
 export class App {
-  protected cartSrv = inject(CartSourceService);
-  protected vatSrv = inject(VatService);
-
-  items = this.cartSrv.cart;
-  vat = this.vatSrv.vat;
-
-  updateItemQuantity(item: CartItem, newQuantity: number) {
-    if (newQuantity === null) {
-      return;
-    }
-    if (newQuantity > 0) {
-      this.cartSrv.setQuantity(item.id, newQuantity);
-    } else {
-      this.cartSrv.removeItem(item.id);
-    }
-  }
 }

6c8f9b413abf5fc02d76a6fad25f5cbc1a1414cf
Enrico Battistella <enrico.battistella@digitalrockers.it>
2026-04-13 16:58:20 +0200

    filter component

--- src/app/pages/product-list/product-list.component.ts
+++ src/app/pages/product-list/product-list.component.ts
@@ -1,18 +1,20 @@
 import { Component, inject } from '@angular/core';
-import { ProductService } from '../../services/product.service';
+import { ProductFilter, ProductService } from '../../services/product.service';
 import { ProductCardComponent } from "../../components/product-card/product-card.component";
 import { VatService } from '../../services/vat.service';
 import { CartSourceService } from '../../services/cart-source.service';
 import { AsyncPipe } from '@angular/common';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { debounceTime, startWith, switchMap } from 'rxjs';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { debounceTime, filter, from, Observable, startWith, Subject, switchMap, tap } from 'rxjs';
+import { ProductFilterComponent, ProductFilterEvent } from '../../components/product-filter/product-filter.component';
 
 @Component({
   selector: 'app-product-list',
   imports: [
     ProductCardComponent,
     AsyncPipe,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    ProductFilterComponent
   ],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css',
@@ -21,24 +23,22 @@ export class ProductListComponent {
   protected productSrv = inject(ProductService);
   protected cartSrv = inject(CartSourceService);
   protected vatSrv = inject(VatService);
-  protected fb = inject(FormBuilder);
+  protected filters$ = new Subject<ProductFilter>();
 
   vat = this.vatSrv.vat;
 
-  filterForm = this.fb.group({
-    name: new FormControl<string | null>(''),
-    minPrice: new FormControl<number | null>(null, { updateOn: 'submit' }),
-    maxPrice: new FormControl<number | null>(null, { updateOn: 'submit' })
-  });
-
-  products = this.filterForm.valueChanges
+  products$ = this.filters$
               .pipe(
                 startWith({}),
                 debounceTime(250),
                 switchMap(filter => this.productSrv.find(filter))
-              )
+              );
 
   add(id: string, quantity: number) {
     this.cartSrv.add(id, quantity);
+  }
+
+  setFilters(filters: ProductFilterEvent) {
+    this.filters$.next(filters);
   }
 }

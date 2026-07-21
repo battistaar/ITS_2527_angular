eb6db88d9bc763e6b00fc174203cf7f2001c3b18
Enrico Battistella <enrico.battistella@digitalrockers.it>
2026-07-21 15:32:42 +0200

    ifAuthenticated directive

--- src/app/pages/product-container/product-container.component.ts
+++ src/app/pages/product-container/product-container.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { SideCartComponent } from '../../components/side-cart/side-cart.component';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { IfAuthenticatedDirective } from '../../utils/if-authenticated.directive';
 
 @Component({
   selector: 'app-product-container',
-  imports: [SideCartComponent, RouterOutlet],
+  imports: [
+    SideCartComponent,
+    RouterOutlet,
+    IfAuthenticatedDirective
+  ],
   templateUrl: './product-container.component.html',
   styleUrl: './product-container.component.css',
+  providers: [AuthService]
 })
-export class ProductContainerComponent {}
+export class ProductContainerComponent {
+}
